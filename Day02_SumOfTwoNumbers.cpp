4c0fd31ec007328796125bcc3349a222d73361a0
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-06-28 08:33:17 +0530

    Day 2: Sum of Two Numbers in C++

---
+++ Day02_SumOfTwoNumbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num1,num2,sum;
+  cout<<"enter first number and second number:";
+  cin>>num1>>num2;
+  sum=num1+num2;
+  cout<<"sum="<<sum<<endl;
+  return 0;
+}
