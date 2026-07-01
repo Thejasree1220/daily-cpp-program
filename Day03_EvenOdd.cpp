f4502ba74e11cf8ffb86f19f8eb420e1373cf28f
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-01 06:59:16 +0530

    Day 3: Even or Odd Program in C++

---
+++ Day03_EvenOdd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num;
+cout<<"enter the num:";
+cin>>num;
+if(num%2==0)
+{
+cout<<num<<"is an even num"<<endl;
+}
+else
+{
+cout<<num<<"is an odd num"<<endl;
+}
+}
