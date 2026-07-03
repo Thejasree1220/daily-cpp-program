3cf47ac6c2fe1b8a6dfb23dcee1cec5a48b76e73
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-03 07:19:43 +0530

    Day 5: Positive, Negative or Zero in C++

---
+++ Day05_PositiveNegativeZero.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num;
+cout<<"enter the num:"<<endl;
+cin>>num;
+if(num<0)
+{
+cout<<num<<" is negative num"<<endl;
+}
+else if(num>0)
+{
+cout<<num<<" is positive num"<<endl;
+}
+else
+{
+cout<<num<<" The num is Zero"<<endl;
+}
+}
