5294b766c6102ccc118ac848a1e35873a6926896
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-12 22:45:01 +0530

    Day 14: Armstrong Number in C++

---
+++ Day14_ArmstrongNumber.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num, originalNum, remainder, result = 0;
+    cout << "Enter a 3-digit integer: ";
+    cin >> num;
+    originalNum = num;
+    while (originalNum != 0)
+    {
+        remainder = originalNum % 10;
+        result += remainder * remainder * remainder;
+        originalNum /= 10;
+    }
+    if (result == num)
+        cout << num << " is an Armstrong Number." << endl;
+    else
+        cout << num << " is not an Armstrong Number." << endl;
+    return 0;
+}
