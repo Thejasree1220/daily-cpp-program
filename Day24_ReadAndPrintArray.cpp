e9644885fdb0146fffd73614c71858354d57bc13
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-07-23 22:17:29 +0530

    Day 24: Read and Print Array Elements in C++
    Fix typo in include directive and ensure proper output formatting.

---
+++ Day24_ReadAndPrintArray.cpp
@@ -0,0 +1,16 @@
+#inlcude<iostream>
+using namespace std;
+int main()
+{
+  int i,n;
+  cout<<"enter array size:";
+  cin>>n;
+  int a[n];
+  cout<<"enter array elements:";
+  for(i=0;i<n;i++)
+    cin>>a[i];
+  for(i=0;i<n;i++)
+    cout<<a[i];
+  cout<<endl;
+ return 0;
+}
