871c2546dce18aa59d0bfc67787bda5fd880979e
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-08-20 22:11:38 +0530

    Day 41: Read and Display a String
    Fix include directive and output stream syntax errors.

---
+++ Day41_StringInput.cpp
@@ -0,0 +1,12 @@
+#include<iostrem>
+using namespace std;
+int main()
+{
+  char s[100];
+cout<<"enter the string:";
+cin>>s;//it is only for single word
+/*If you want to scan multiple words then use
+cin.getline(s,100);
+*/
+cout<<"given string is:"<<s<<endl;
+}
