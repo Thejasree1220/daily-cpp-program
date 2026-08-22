37a5bfb6576bafe762bfbce16e9fc8c7669a4920
Nemalla Thejasree <nemallatejasree@gmail.com>
2026-08-22 11:08:20 +0530

    Add program to count vowels in a string

---
+++ Day43_CountVowelsInString.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        char s[100];
+        int v=0,i;
+        cout<<"enter the string:";
+        cin.getline(s,100);
+        for(i=0;s[i];i++)
+        {
+                if((s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')||(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'))
+                        v++;
+        }
+        cout<<"count of vowels is:"<<v<<endl;
+}
